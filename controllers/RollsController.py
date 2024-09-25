53c4e0beee0ee70000bf1f74f3f0d306ea16e2ee
peter <petermy666@gmail.com>
2024-09-25 17:11:24 +0400

    Добавлен файл RollsController.py написан класс RollsController, добавлен конструктор, 2 представления, сеттеры с проверкой на права доступа. Подкорректирован класс RollsModel в файле RollsModel.py.

---
+++ controllers/RollsController.py
@@ -0,0 +1,49 @@
+class RollsController:
+
+    def __init__(self, model):
+        self.model = model
+
+    def restaurant_menu(self):
+        roll_data = (f'Роллы : {self.model.get_name()}\n'
+                     f'Вес : {self.model.get_weight()}'
+                     f'Количество : {self.model.get_quantity()}\n'
+                     f'Цена : {self.model.get_price()}\n'
+                     f'Состав : {', '.join(self.model.get_ingredients())}')
+        return roll_data
+
+    def site_restaurant_menu(self):
+        roll_data = (f'Роллы : {self.model.get_name()}\n'
+                     f'Вес 1 штуки : {self.model.get_weight()}'
+                     f'Количество : {self.model.get_quantity()}\n'
+                     f'Цена : {self.model.get_price()}\n'
+                     f'Состав : {', '.join(self.model.get_ingredients())}'
+                     f'Фото : {self.model.get_picture()}')
+        return roll_data
+
+    def set_ingredients(self,user_rights, new_ingredients: list):
+        if user_rights in ['Director','Admin','IsStaff']:
+            self.model.set_ingredients(new_ingredients)
+            return 'Рецепт изменен!'
+        else:
+            return 'У вас нет права доступа :('
+
+    def set_quantity(self, user_rights, new_quantity: int):
+        if user_rights in ['Director', 'Admin', 'IsStaff']:
+            self.model.set_quantity(new_quantity)
+            return 'Количество изменено!'
+        else:
+            return 'У вас нет права доступа :('
+
+    def set_weight(self, user_rights, new_weight: int):
+        if user_rights in ['Director', 'Admin', 'IsStaff']:
+            self.model.set_weight(new_weight)
+            return 'Вес изменён!'
+        else:
+            return 'У вас нет права доступа :('
+
+    def set_price(self, user_rights, new_price: int):
+        if user_rights in ['Director', 'Admin', 'IsStaff']:
+            self.model.set_price(new_price)
+            return 'Цена измененa!'
+        else:
+            return 'У вас нет права доступа :('
--- models/RollsModel.py
+++ models/RollsModel.py
@@ -1,8 +1,10 @@
-class Rolls:
+class RollsModel:
 
-    def __init__(self, name: str, ingredients: list, quantity: int, price: float, client_ingredients: list = None):
+    def __init__(self, name: str, ingredients: list, weight: float, quantity: int, price: float,
+                 client_ingredients: list = None):
         self.__name = name
         self.__ingredients = ingredients
+        self.__weight = weight
         self.__quantity = quantity
         self.__price = price
         if client_ingredients is not None:
@@ -16,6 +18,9 @@ class Rolls:
     def get_ingredients(self):
         return self.__ingredients
 
+    def get_weight(self):
+        return self.__weight
+
     def get_quantity(self):
         return self.__quantity
 
@@ -25,18 +30,24 @@ class Rolls:
     def get_client_ingredients(self):
         return self.__client_ingredients
 
-    def set_ingredients(self,new_ingredients: list):
+    def set_ingredients(self, new_ingredients: list):
         if type(new_ingredients) is list:
             self.__ingredients.clear()
             self.__ingredients.extend(new_ingredients)
         else:
             return 'Неверный ввод. Тип данных должен быть \'list\''
 
-    def set_quantity(self,new_quantity: int):
+    def set_quantity(self, new_quantity: int):
         if type(new_quantity) is int:
             self.__quantity = new_quantity
         else:
             return 'Неверный ввод. Тип данных должен быть \'int\''
+
+    def set_weight(self, new_weight: float):
+        if type(new_weight) is float:
+            self.__weight = new_weight
+        else:
+            return 'Неверный ввод. Тип данных должен быть \'float\''
 
     def set_price(self, new_price: float):
         if type(new_price) is float:
